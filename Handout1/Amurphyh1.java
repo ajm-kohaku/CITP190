57b25715ff18a60d31fa45e4a0c68b1ddb2176ef
Niertis Evani <niertis@gmail.com>
2013-09-05 20:29:29 -0400

    1st assignment
    new Hello World! Java

---
+++ Handout1/Amurphyh1.java
@@ -0,0 +1,42 @@
+/*
+ASCII Signagure: AJM
+                 ,.     ,.
+                {^ \-"-/ ^}
+                "   """   "
+               { <O> _ <O> }
+               ==_ .:Y:. _==
+             .""  `--^--' "".
+            (,~-~."" "" ,~-~.)
+      ------(     )----(     )-----
+            ^-'-'-^    ^-'-'-^
+      _____________________________
+            |"""" /~.^.~\ """"|
+      hjw    ,i-i-i(""(  i-i-i.
+      `97   (o o o ))"")( o o o)
+             \(_) /(""(  \ (_)/
+              `--'  \""\  `--'
+                     )"")
+                    (""/
+                     `"                            
+*
+ */
+package amurphyh1;
+
+/**
+Programmer: Amber Murphy
+Course:     CITP 190: Fall 2013
+Filename:   Amurphyh1.java
+Assignment: Handout1
+Summary:    Create print line of hello world!
+ */
+public class Amurphyh1 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println
+                ("Hello World!");
+    }
+}
