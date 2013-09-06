d3afae56ac511ab15f98caa4ec4d35c555378319
Niertis Evani <niertis@gmail.com>
2013-09-05 20:35:25 -0400

    making assignment 1 prettier

--- Handout1/Amurphyh1.java
+++ Handout1/Amurphyh1.java
@@ -1,7 +1,7 @@
 /*
 ASCII Signagure: AJM
                  ,.     ,.
-                {^ \-"-/ ^}
+                {^ \-"-/ ^}             
                 "   """   "
                { <O> _ <O> }
                ==_ .:Y:. _==
@@ -18,22 +18,18 @@ ASCII Signagure: AJM
                      )"")
                     (""/
                      `"                            
-*
- */
+************************************************
+* Programmer: Amber Murphy
+* Course:     CITP 190: Fall 2013
+* Filename:   Amurphyh1.java
+* Assignment: Handout1
+* Summary:    Create print line of hello world!
+************************************************
+*/
 package amurphyh1;
 
-/**
-Programmer: Amber Murphy
-Course:     CITP 190: Fall 2013
-Filename:   Amurphyh1.java
-Assignment: Handout1
-Summary:    Create print line of hello world!
- */
 public class Amurphyh1 
 {
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) 
     {
         System.out.println
